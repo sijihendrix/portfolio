182f2f0dd81c6fdd92ff0423bff740e0f4d7e3c3
siji <sijibamgboye@yahoo.com>
2021-05-17 21:27:48 +0100

    Trigger build

--- src/comps/reusables/nav/Socials.tsx
+++ src/comps/reusables/nav/Socials.tsx
@@ -2,14 +2,11 @@ import React from "react";
 import { useStyles } from "./styles";
 import { ReactComponent as GitHub } from "../../../assets/github.svg";
 import { ReactComponent as Linkedin } from "../../../assets/linkedin.svg";
-import { Toggle } from "../toggle-switch/Toggle";
 
 export const Socials = () => {
   const classes = useStyles();
   return (
     <main className={classes.socials}>
-      {/* <Toggle /> */}
-
       <a
         href="https://www.linkedin.com/in/oluwasijibomi-bamgboye/"
         target="_blank"
